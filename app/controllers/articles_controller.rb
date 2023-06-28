ec2d2960c27ab007247d450ebc5f3d1cd09675af
Tina Shi <init.defaultBranch>
2023-06-28 21:54:50 +1000

    commit tree d8b4768d12204ba202da6c381b1090d03a48ee62

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,9 +7,16 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    session[:page_views] ||= 0
     article = Article.find(params[:id])
-    render json: article
+    if session[:page_views] < 4
+      session[:page_views] = session[:page_views] + 1
+      render json: article
+    else
+      render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+    end
   end
+
 
   private
 
